c33f88c32875b580d4118018df75bc3f0599b2d9
Tinky <48660658+Tinky124@users.noreply.github.com>
2020-12-12 14:54:01 +0100

    No ESX on client

--- cl_main.lua
+++ cl_main.lua
@@ -1,11 +1,3 @@
-ESX = nil
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-end)
-
 function snijegon() -- Tinky: esx_balkan: https://discord.gg/KbEgsV4tjw
     SetForceVehicleTrails(true)
     SetForcePedFootstepsTracks(true)
@@ -13,9 +5,7 @@ function snijegon() -- Tinky: esx_balkan: https://discord.gg/KbEgsV4tjw
     RequestScriptAudioBank("ICE_FOOTSTEPS", false)
     RequestScriptAudioBank("SNOW_FOOTSTEPS", false)
     RequestNamedPtfxAsset("core_snow")
-    while not HasNamedPtfxAssetLoaded("core_snow") do
-        Wait(0)
-    end
+    while not HasNamedPtfxAssetLoaded("core_snow") do Wait(0) end
     UseParticleFxAssetNextCall("core_snow")
 end
 
