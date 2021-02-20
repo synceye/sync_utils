fba2328331232b247b5d6151ae5d00e986aab9e1
SyncEye <50116007+DamjanPetrovicki@users.noreply.github.com>
2021-02-20 21:47:41 +0100

    removed snow

--- cl_main.lua
+++ cl_main.lua
@@ -88,24 +88,6 @@ end)
         end
     end
 
-
-function snijegon() -- Tinky: esx_balkan: https://discord.gg/KbEgsV4tjw
-    SetForceVehicleTrails(true)
-    SetForcePedFootstepsTracks(true)
-    ForceSnowPass(true)
-    RequestScriptAudioBank("ICE_FOOTSTEPS", false)
-    RequestScriptAudioBank("SNOW_FOOTSTEPS", false)
-    RequestNamedPtfxAsset("core_snow")
-    while not HasNamedPtfxAssetLoaded("core_snow") do Wait(0) end
-    UseParticleFxAssetNextCall("core_snow")
-end
-
-function snijegoff() -- Tinky: esx_balkan: https://discord.gg/KbEgsV4tjw
-    SetForceVehicleTrails(false)
-    SetForcePedFootstepsTracks(false)
-    ForceSnowPass(false)
-end
-
 local mp_pointing = false --> ALL: https://github.com/redoper1/FiveM-Point-finger/
 local keyPressed = false
 
